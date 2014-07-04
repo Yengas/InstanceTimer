86f69f34f863837f8abe9830effcaf91f91249cb
Xploder <Xploder270@gmail.com>
2014-07-04 19:13:54 +0200

    Update deDE.lua
    German translation
    
    xploder270/curse.com

--- deDE.lua
+++ deDE.lua
@@ -1,7 +1,7 @@
 local L = Apollo.GetPackage("Gemini:Locale-1.0").tPackage:NewLocale("InstanceTimer", "deDE")
 if not L then return end
-L["loadError"] = "Could not load the main window for some reason."
-L["failedMessage"] = "You have failed the time requirement for %s."
-L["spentFormat"] = "%i hours, %i minutes, %i seconds"
-L["spentResponse"] = "You have been in %s for %s."
-L["notInInstance"] = "You're not in an instance."
+L["loadError"] = "Das Hauptfenster konnte nicht geladen werden."
+L["failedMessage"] = "Zeitlimit für %s überschritten."
+L["spentFormat"] = "%i Stunden, %i Minuten, %i Sekunden"
+L["spentResponse"] = "Du befindest dich seit %s in %s." -- first %s = time second %s = location
+L["notInInstance"] = "Du befindest dich nicht in einer Instanz."
